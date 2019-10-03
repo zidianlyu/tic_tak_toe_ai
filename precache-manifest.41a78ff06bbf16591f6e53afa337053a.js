57848b9e00cbe281c8dc80d0873d364a4e0f0413
Zidian Lyu <zlu@ucdavis.edu>
2019-10-02 21:18:55 -0700

    upload to git

---
+++ precache-manifest.41a78ff06bbf16591f6e53afa337053a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0861be37717859052851c78f798d4e26",
+    "url": "/tic_tak_toe_ai/index.html"
+  },
+  {
+    "revision": "2a27f84c2431ac04cd74",
+    "url": "/tic_tak_toe_ai/static/js/2.210f98ef.chunk.js"
+  },
+  {
+    "revision": "2c23aaf48388d9cbe913",
+    "url": "/tic_tak_toe_ai/static/js/main.f793d5a3.chunk.js"
+  },
+  {
+    "revision": "1d033f93bb2e1314e980",
+    "url": "/tic_tak_toe_ai/static/js/runtime-main.71993933.js"
+  }
+]);
